c4c224097764bf03ea7547f7b07712a46cd15141
Zach Selindh <zachselindh@gmail.com>
2020-03-31 13:05:36 -0500

    Added onShowClick method.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-function Contact(props) {
-  const { name, email, phone } = props.contact;
-  return (
-    <div className="card card-body mb-3">
-      <h4>{name}</h4>
-      <ul className="list-group">
-        <li className="list-group-item">Email: {email}</li>
-        <li className="list-group-item">Phone: {phone}</li>
-      </ul>
-    </div>
-  );
+class Contact extends Component {
+  state = {};
+  onShowClick = e => {
+    console.log(e.target);
+  };
+
+  render() {
+    const { name, email, phone } = this.props.contact;
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name}{" "}
+          <i onClick={this.onShowClick} className="">
+            +
+          </i>
+        </h4>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 Contact.propTypes = {
