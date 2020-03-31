db36ed0a83130af42c5a7d2d8d9e4f1d46daeae5
Zach Selindh <zachselindh@gmail.com>
2020-03-31 12:15:15 -0500

    Added bootstrap for styling.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import ContactList from "./components/ContactList";
+/* import ContactList from "./components/ContactList"; */
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <Contact name="Joe" email="joe@test.com" phone="(281)-555 5555" />
-      <Contact name="Karen" email="karen@test.com" phone="(281)-555-5556" />
+      <div className="container">
+        <Contact name="Joe" email="joe@test.com" phone="(281)-555 5555" />
+        <Contact name="Karen" email="karen@test.com" phone="(281)-555-5556" />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,21 @@ import PropTypes from "prop-types";
 function Header(props) {
   const { branding } = props;
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item"></li>
+            <a href="/" className="nav-link">
+              Home
+            </a>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
