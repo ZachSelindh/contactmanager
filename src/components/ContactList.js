37be4e4d76fe433cf397ddd5632f4f6d855fbd38
Zach Selindh <zachselindh@gmail.com>
2020-04-01 16:27:39 -0500

    Added Reducer for deleting contacts via Context API.

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,14 +4,6 @@ import Contact from "./Contact";
 import { Consumer } from "../context";
 
 class ContactList extends Component {
-  deleteContact = id => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: newContacts
-    });
-  };
-
   render() {
     return (
       <Consumer>
@@ -20,11 +12,7 @@ class ContactList extends Component {
           return (
             <React.Fragment>
               {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-                />
+                <Contact key={contact.id} contact={contact} />
               ))}
             </React.Fragment>
           );
