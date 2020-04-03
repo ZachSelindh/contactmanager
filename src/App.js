8c401a0640a0e5e958df76893455ccb90a4c3aaa
Zach Selindh <zachselindh@gmail.com>
2020-04-03 13:45:02 -0500

    Added react-router-dom and implemented pages on App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import ContactList from "./components/contacts/ContactList";
 import Header from "./components/layout/Header";
-
 import AddContact from "./components/contacts/AddContact";
+import About from "./components/pages/About";
 
 import { Provider } from "./context";
 
@@ -11,13 +14,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <Provider>
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <AddContact />
-          <ContactList />
+      <Router>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={ContactList} />
+              <Route exact path="/contact/add" component={AddContact} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     </Provider>
   );
 }
